a067ca274b5c84198dee99597dc2bc0952504b25
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-03 13:26:05 +0300

    update verification email

--- src/utils/nodemailer/nodemailer.js
+++ src/utils/nodemailer/nodemailer.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS,
