57799381ee5af89718d52de903b6583be3067ce7
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-03 14:49:01 +0300

    update dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express"
 import { bootstrap } from "./src/bootstrap.js";
 import { stripeWebhook } from "./src/modules/controler/payment_controler.js";
+import dotenv from "dotenv";
+dotenv.config(); 
 
 const app = express()
 app.post(
