d14d7fe6d8b00beaa6f494c81dc63883f8fbaf09
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-04 13:15:59 +0300

    uop

--- src/utils/nodemailer/nodemailer.js
+++ src/utils/nodemailer/nodemailer.js
@@ -9,8 +9,8 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
 
 host: "smtp-relay.brevo.com",
- port: 587,
-  secure: false, 
+ port: 465,
+  secure: true, 
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASS, // اتأكد إنه الكود الـ 16 حرف الجديد
