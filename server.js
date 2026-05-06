d5f0f7aefc5cc4a196217e1aeaa7d660e637f639
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-06 15:25:04 +0300

    add morgen

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ import express from "express"
 import { bootstrap } from "./src/bootstrap.js";
 import { stripeWebhook } from "./src/modules/controler/payment_controler.js";
 import dotenv from "dotenv";
+import morgan from "morgan";
 dotenv.config(); 
 
 const app = express()
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev')); 
+}
 app.post(
   "/webhook", 
   express.raw({ type: "application/json" }), 
