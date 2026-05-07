6ae524c2cd93689699f070830e61d594fc744b3e
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-07 11:08:58 +0300

    update winston

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,42 +1,37 @@
-import winston from 'winston';
-import 'winston-daily-rotate-file';
-
+import winston from "winston";
+import "winston-daily-rotate-file";
 
 const logFormat = winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.printf((info) => `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`)
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(
+    (info) =>
+      `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`,
+  ),
 );
 
 const errorRotateTransport = new winston.transports.DailyRotateFile({
-    filename: 'logs/error-%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    maxFiles: '14d',
-    level: 'error', 
+  filename: "logs/error-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: "14d",
+  level: "error",
 });
 
 const combinedRotateTransport = new winston.transports.DailyRotateFile({
-    filename: 'logs/combined-%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    maxFiles: '7d',
+  filename: "logs/combined-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: "7d",
 });
-
 
 const logger = winston.createLogger({
-    level: 'info', 
-    format: logFormat,
-    transports: [
-        errorRotateTransport,
-        combinedRotateTransport,
-    ],
+  level: "info",
+  format: logFormat,
+  transports: [errorRotateTransport, combinedRotateTransport],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.combine(
-            winston.format.colorize(), 
-            logFormat
-        ),
-    }));
-}
+logger.add(
+  new winston.transports.Console({
+    format: winston.format.combine(winston.format.colorize(), logFormat),
+  }),
+);
 
 export default logger;
