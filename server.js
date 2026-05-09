5f031aa9413877d3a64c7faa75f7f99b26074e3b
alimumd177437432-a11y <alimumd177437432@gmail.com>
2026-05-09 15:14:42 +0300

    update

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import { bootstrap } from "./src/bootstrap.js";
 import { stripeWebhook } from "./src/modules/controler/payment_controler.js";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 import helmet from "helmet";
 dotenv.config(); 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
-app.use('/public', express.static('public'));
+app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.use(helmet())
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev')); 
